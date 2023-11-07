51b44a845ed6970ee0badf5804904f506e4adabf
Matteo Falasca <matteo.falasca@emmek.org>
2023-11-07 14:39:04 +0100

    firt commit

---
+++ src/main/java/org/emmek/beu2w2d2/BeU2W2D2Application.java
@@ -0,0 +1,13 @@
+package org.emmek.beu2w2d2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeU2W2D2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeU2W2D2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/emmek/beu2w2d2/BeU2W2D2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.emmek.beu2w2d2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeU2W2D2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
